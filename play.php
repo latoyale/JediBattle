9053103ae2f7fc1243ffcdb3eedc754c5f98b6aa
LaToya Legemah <llegemah@gmail.com>
2016-11-08 17:09:16 -0500

    initial commit
    initial commit

---
+++ play.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once __DIR__.'/lib/Ship.php';
+
+/**
+ * @param Ship $someShip
+ */
+
+function printShipSummary($someShip){
+    echo 'Ship name: ' .$someShip->getname();
+    echo '<hr/>';
+    $someShip->sayHello();
+    echo '<hr/>';
+    echo $someShip->getName();
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(false);
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(true);
+}
+
+$myShip = new Ship();
+$myShip->getname() = 'Jedi Starship';
+$myShip->weaponPower = 10;
+
+
+
+$otherShip = new Ship();
+$otherShip->getname()= 'Imperial Shuttle';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
+
+echo '</hr>';
+printShipSummary($myShip);
+echo '</hr>';
+printShipSummary($otherShip);
+echo '</hr>';
+if($myShip->doesGivenShipHaveMoreStrength($otherShip)) {
+    echo $myShip->name .' has more strength';
+} else {
+    echo $otherShip->name .' has more strength';
+}
