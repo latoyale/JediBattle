13a3bd8631856488bfec4f8ca3b986b86143f3fe
LaToya Legemah <llegemah@gmail.com>
2016-11-09 02:26:04 -0500

    initial upload
    inital upload

--- index.php
+++ index.php
@@ -81,10 +81,9 @@ if (isset($_GET['error'])) {
         <?php endforeach; ?>
         </tbody>
     </table>
-
     <div class="battle-box center-block border">
         <div>
-            <form method="POST" action="/battle.php">
+            <form method="POST" action="/JediBattle/battle.php">
                 <h2 class="text-center">The Mission</h2>
                 <input class="center-block form-control text-field" type="text" name="ship1_quantity" placeholder="Enter Number of Ships" />
                 <select class="center-block form-control btn drp-dwn-width btn-default btn-lg dropdown-toggle" name="ship1_name">
